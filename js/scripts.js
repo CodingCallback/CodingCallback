458a7746e09bf3506e5bb2b14f66c1738095db10
SteveMc <contact@stevemcarthur.co.uk>
2018-09-05 06:41:26 +0100

    added metalsmith article - separate out prism js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(function() {
+   var img = "<image src='/img/folder.png'>";
+    var keywords = $("pre[data-code='filesystem'] .keyword");
+    keywords.text("xxxx");
+    //keywords.append(img);
+});
